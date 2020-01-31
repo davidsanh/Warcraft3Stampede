7ea3ed5c85c8a8dbb6d66cbed2fda281a13fe1bf
Daitli <davidsanh@hotmail.com>
2020-01-30 21:49:19 -0500

    Add files via upload

---
+++ StampedeObjectScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StampedeObjectScript : MonoBehaviour
+{
+    public bool isDead = false, debug;
+    public float movementSpeed, radius, deadTimer, travelDistance;
+
+    [HideInInspector]
+    public float yOffset;
+    [HideInInspector]
+    public LayerMask layerMask;
+    private Vector3 spawnLoc;
+    private float height;
+    [HideInInspector]
+    public BoxCollider spawnCol;
+    private ParticleSystem impactFX, loopFX;
+    private MeshRenderer rend;
+
+    private void Start()
+    {
+        //The sphere mesh renderer
+        rend = GetComponent<MeshRenderer>();
+        //Reference both of our particle systems
+        impactFX = transform.Find("ImpactFX").GetComponent<ParticleSystem>();
+        loopFX = transform.Find("LoopFX").GetComponent<ParticleSystem>();
+        //Store the spawn location to calculate travel distance
+        spawnLoc = transform.position;
+        //Set the height of our raycast
+        height = spawnCol.size.y + 1;
+    }
+
+    private void Update()
+    {
+        if (!isDead)
+        {
+            //position and movement
+            RaycastHit hit;
+            var curPosition = transform.position;
+            //Split it half using the height of the collider
+            curPosition.y += height / 2;
+            //Begin raycast
+            if (Physics.Raycast(curPosition, Vector3.down, out hit, height, layerMask))
+            {
+                //Add an offset so it is not inside the ground
+                var yPos = hit.point.y + yOffset;
+                //Set our position accordingly
+                transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+                //Move this object forward using the movement speed
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+            else
+            {
+                //We were not able to find a floor so we destroy ourselves
+                Destroy(gameObject);
+            }
+
+            var distanceFromSpawn = Vector3.Distance(transform.position, spawnLoc);
+            if (distanceFromSpawn > travelDistance)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy" && !isDead)
+        {
+            Impact();
+        }
+    }
+
+    void Impact()
+    {
+        impactFX.Play();
+        loopFX.Stop();
+
+        Collider[] objectsInRange = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider col in objectsInRange)
+        {
+            if (col.tag == "Enemy")
+            {
+                Destroy(col.gameObject);
+            }
+        }
+
+        rend.enabled = false;
+        Destroy(gameObject, deadTimer);
+        isDead = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (debug && isDead)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+    }
+}
---
+++ StampedeScript.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StampedeScript : MonoBehaviour
+{
+    //Duration is the lifetime of the stampede, spawnRate is how often they will spawn and SpawnYOffset is the offset on the Y axis upon spawn
+    public float duration, spawnRate, spawnYOffset;
+    //Set this to our stampede obj prefab
+    public GameObject stampedeOBJ;
+    //What will our raycast interact with(ground)
+    public LayerMask layerMask;
+
+    //Private variables, no need to touch these
+    [HideInInspector]
+    public BoxCollider col;
+    private float spawnRateTimer, colliderHeight;
+
+    private void Start()
+    {
+        //Set the spawn timer
+        spawnRateTimer = spawnRate;
+        //Reference the start box collider
+        col = transform.Find("Start").GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        //If our duration is bigger than zero
+        if(duration > 0)
+        {
+            if (spawnRateTimer > 0)
+            {
+                spawnRateTimer -= Time.deltaTime;
+            }
+            else { SpawnObject(); }
+
+            duration -= Time.deltaTime;
+        }
+        else
+        {
+            //Get all our children that contain a transform
+            Transform[] allChildren = GetComponentsInChildren<Transform>();
+            foreach (Transform child in allChildren)
+            {
+                //Make sure we are destroying the correct children by checking their name
+                if(child.name == "StampedeOBJ")
+                {
+                    child.SetParent(null);
+                }
+            }
+            //Destroy our stampede
+            Destroy(gameObject);
+        }
+    }
+
+    void SpawnObject()
+    {
+        //Set timer
+        spawnRateTimer = spawnRate;
+
+        //Getting spawn location
+        RaycastHit hit;
+        colliderHeight = col.size.y + 1;
+        var randomPosition = RandomPointInsideBox(col.bounds);
+        randomPosition.y = col.transform.position.y + (colliderHeight / 2);
+
+        if (Physics.Raycast(randomPosition, Vector3.down, out hit, colliderHeight, layerMask))
+        {
+            var yPos = hit.point.y + spawnYOffset;
+            Vector3 spawnPosition = new Vector3(randomPosition.x, yPos, randomPosition.z);
+
+            var obj = Instantiate(stampedeOBJ, spawnPosition, Quaternion.identity);
+            obj.transform.SetParent(transform);
+            obj.name = "StampedeOBJ";
+
+            var stampedeScript = obj.GetComponent<StampedeObjectScript>();
+            stampedeScript.spawnCol = col;
+            stampedeScript.layerMask = layerMask;
+            stampedeScript.yOffset = spawnYOffset;
+        }
+    }
+
+    //This function gives you a random location within some bounds
+    public static Vector3 RandomPointInsideBox(Bounds bounds)
+    {
+        return new Vector3
+        (
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
+    }
+}
